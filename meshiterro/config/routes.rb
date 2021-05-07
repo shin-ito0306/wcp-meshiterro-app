fa34d1587bab2d3293e79d922baec96c1bcfac6f
shin-ito0306 <shin.20000306@icloud.com>
2021-05-07 09:08:33 +0000

    [add] postimages new

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
+  resources :post_images, only: [:index, :new, :create, :show, :destroy]
+
 end
