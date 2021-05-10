4fc1626337628d6c489b16dacedd440061537991
shin-ito0306 <shin.20000306@icloud.com>
2021-05-10 09:11:02 +0000

    [finish] 5-6

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,excpt: []
 
   def after_sign_in_path_for(resource)
     post_images_path
